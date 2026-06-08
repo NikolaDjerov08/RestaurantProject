a1ffea3ceb19de943a5022a98de8f7b5fc884690
NikolaDjerov <126071907+NikolaDjerov@users.noreply.github.com>
2026-06-08 09:18:10 +0300

    com

---
+++ RestaurantProject/RestaurantProject.Services/Constants/PolicyConstants.cs
@@ -0,0 +1,7 @@
+namespace Restaurant.Core.Constants;
+
+public static class PolicyConstants
+{
+    public const string AdminOnly = "AdminOnly";
+    public const string StaffOnly = "StaffOnly"; // Admin or Employee
+}
--- RestaurantProject/RestaurantProject.Services/Contracts/IAdminService.cs
+++
@@ -1,8 +0,0 @@
-using Restaurant.Core.Models.Admin;
-
-namespace Restaurant.Core.Contracts;
-
-public interface IAdminService
-{
-    Task<DashboardStatsModel> GetDashboardStatsAsync();
-}
